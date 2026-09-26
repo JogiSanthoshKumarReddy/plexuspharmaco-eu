2ef90fd08dd563394a18b2bac28aca51d1b0438e
Guest <jogisanthosh17@gmail.com>
2026-09-26 16:42:59 +0530

    chore: set unoptimized images for Cloudflare Pages

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     optimizePackageImports: ['lucide-react', 'framer-motion'],
   },
   images: {
+    unoptimized: true,
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 31536000,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
